40676138efac1f04a551ed8f3b571c110fec6c23
kaltechCode <kaltechconsultancy@gmail.com>
2026-07-24 08:27:49 -0500

    order

--- src/app/(account)/account/orders/page.tsx
+++ src/app/(account)/account/orders/page.tsx
@@ -1,15 +1,7 @@
-import OrdersPage from "@/components/order/OrderPage";
+import React from "react";
 
-export const metadata = {
-  title: "My Orders",
-};
-
-export default function Page() {
-  return (
-    <div className="space-y-8">
-      <h1 className="text-3xl font-bold">My Orders</h1>
-
-      <OrdersPage />
-    </div>
-  );
+function page() {
+  return <div>page</div>;
 }
+
+export default page;
